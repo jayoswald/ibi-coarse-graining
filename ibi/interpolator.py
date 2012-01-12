252100f3a0d9c6cda4d1d5404f6c176ee4b3d5c8
jay.oswald <jay.oswald@gmail.com>
2012-01-12 01:26:11 +0000

    Wrote a new version of interpolator that handles the end points better.

--- ibi/interpolator.py
+++ ibi/interpolator.py
@@ -4,54 +4,52 @@
     object that can return the interpolated value or its derivative at any point
     within [min(x), max(x)].
 """
-from numpy import linspace, sin
+from numpy import *
 
 class Interpolator:
+    # Initializes the spline coefficients.
+    # see http://mathworld.wolfram.com/CubicSpline.html for details.
     def __init__(self, x, y):
-        self.intervals = intervals = []
-        # Generate a cubic spline for each interpolation interval.
-        for i in range(len(x)-1): 
-            u,v   = x[i],x[i+1]
-            FU,FV = y[i],y[i+1] 
-            # adjust h as needed
-            h = 0.001
-            if i>0:
-                DU = (y[i+1]-y[i-1]) / (x[i+1]-x[i-1])
-            else:
-                DU = (y[i+1]-y[i]) / (x[i+1]-x[i])
+        rhs       = zeros((len(x)))
+        rhs[0]    = y[1]-y[0]
+        rhs[1]    = y[2]-y[0]
+        rhs[2:-2] = y[3:-1] - y[1:-3]
+        rhs[-2]   = y[-1] - y[-3]
+        rhs[-1]   = y[-1] - y[-2]
 
-            if i<len(x)-2:
-                DV = (y[i+2]-y[i]) / (x[i+2]-x[i])
-            else:
-                DV = (y[i+1]-y[i]) / (x[i+1]-x[i])
+        n  = len(rhs)
+        A  = diag([4.0]*n, 0)
+        A += diag([1.0]*(n-1), -1) + diag([1.0]*(n-1),  1)
+        A[0,0] = A[-1,-1] = 2.0
+        self.d = linalg.solve(A, 3.0*rhs)
+        self.x = copy(x)
+        self.y = copy(y)
+        self.npts = len(self.d)-1
 
-            denom = (u - v)**3
-
-            A = (u-v)*(DV+DU) + 2*(FV-FU)
-            B = -(v*v*(-DV - 2*DU) +
-                  u*(v*(DU-DV) + 3*(FV-FU)) + 3*v*(FV-FU) + u*u*(2*DV + DU)) 
-            C = (- DU*v**3 + u*(v*v*(-2*DV-DU)  + 6*v*(FV-FU)) +
-                 v*u*u*(DV + 2*DU) + DV*u**3) 
-            D = -(u*(-DU*v**3  - 3*FU*v*v) +
-                  FU*v**3 + u*u*(v*v*(DU-DV) + 3*FV*v) + u**3 * (DV*v-FV)) 
-            intervals.append((u, A/denom, B/denom, C/denom, D/denom))
-
+    # Computes the interpolated function.
     def __call__(self, x):
-        u, A, B, C, D = self.get_interval(x, 0, len(self.intervals))
-        # Plug coefficients into polynomial.
-        return ((A*x + B)*x + C)*x + D
+        xi,a,b,c,d = self.get_interval(x, 0, self.npts)
+        return a + b*xi + c*xi*xi + d*xi*xi*xi
 
+    # Computes the derivative of the interpolated function.
     def derivative(self,x):        
-        u, A, B, C, D = self.get_interval(x, 0, len(self.intervals))
-        return 3*A*x*x + 2*B*x + C
+        xi,a,b,c,d = self.get_interval(x, 0, self.npts)
+        return (3.0*d*xi*xi + 2.0*c*xi + b)/(self.x[1]-self.x[0])
 
-    # Tree-search the intervals to get coefficients.
+    # Binary search the intervals to get coefficients.
     def get_interval(self, x, beg, end):
         n = end-beg
         if n < 2: 
-            return self.intervals[beg]
+            i = beg
+            xi = (x - self.x[i]) / (self.x[i+1] - self.x[i])
+            a = self.y[i]
+            b = self.d[i]
+            c = 3.0*(self.y[i+1] - self.y[i])   - 2.0*b - self.d[i+1]
+            d = 2.0*(self.y[i]   - self.y[i+1]) +     b + self.d[i+1]
+            return (xi,a,b,c,d)
+
         mid = beg + n/2
-        if x < self.intervals[mid][0]: 
+        if x < self.x[mid]: 
             return self.get_interval(x, beg, mid)
         else: 
             return self.get_interval(x, mid, end)
